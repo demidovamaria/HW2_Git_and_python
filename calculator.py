324a2f943ef1c8fec87ad0a36940166256c493d2
Your Name <masha@kompuk.localdomain>
2025-09-19 10:56:43 +0300

    Add subtraction function

--- calculator.py
+++ calculator.py
@@ -13,7 +13,8 @@ def main():
 
     if sign == '/':
         result = divide(num1, num2)
-    #     Коллеги, ниже добавьте свои методы(+,-,*)
+    elif sign == '-':
+        result = subtract(num1, num2)
     else:
         print("Invalid operator")
         return
@@ -26,6 +27,9 @@ def divide(num1, num2):
     if num2 == 0:
         raise ValueError("Division by zero is not allowed")
     return num1 / num2
+
+def subtract(num1, num2):
+    return num1 - num2
 
 while True:
     main()
